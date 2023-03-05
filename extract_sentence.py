abef6ea24e2aaf49439c5643f654b387e667ddf0
Cappuccinomild <rkdquddh1208@naver.com>
2023-03-05 13:19:18 +0900

    '.'

--- extract_sentence.py
+++ extract_sentence.py
@@ -1,25 +1,39 @@
 import re
 import numpy as np
 import pandas as pd
-from multiprocessing import Pool
+from multiprocessing import Process, Queue
 import os
 import sys
 from tqdm import tqdm
+import time
 
-def run_process(map_val):
+def walk_file(fname_q):
 
-    df_col = ['file','link','-10', '-9', '-8', '-7', '-6', '-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    for (path, dir, file) in tqdm(os.walk('Search')):
 
-    search_num = map_val['argv']
-    keyword_list = map_val['keyword']
-    file_list = map_val['flist']
+        for fname in file:
+            fname_q.put([path, fname])
+
+    for i in range(10):
+        fname_q.put([])
+
+def run_process(fname_q, result_q, keyword_list):
+
+    df_col = ['file', 'link', 'keyword', '-10', '-9', '-8', '-7', '-6', '-5', '-4', '-3', '-2', '-1', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     
     #result list 초기화
-    df_result = []
-    for k in keyword_list:
-        df_result.append(pd.DataFrame(columns = df_col))
-    
-    for path, fname in file_list:
+    df_result = pd.DataFrame(columns = df_col)
+    while True:
+        
+        flag = fname_q.get()
+        #print(flag)
+        #파일을 다 읽어서 빈 리스트
+        if not flag:
+            return
+        
+        else:
+            path, fname = flag
+
 
         #파일을 읽어서 공백 제거
         text = read_file(path, fname)
@@ -45,7 +59,6 @@ def run_process(map_val):
         text = text.split(".")
         text = list(filter(bool, text))
         
-        i = 0
         for keyword in keyword_list:
             #키워드 추출
             col_insert = extract_sentences(text, keyword)
@@ -54,6 +67,9 @@ def run_process(map_val):
             if col_insert:
 
                 for col in col_insert:
+                    
+                    #결과물에 검색 키워드 입력
+                    col.insert(0, keyword)
 
                     #결과물에 원본링크 입력
                     col.insert(0, link)
@@ -61,11 +77,10 @@ def run_process(map_val):
                     #결과물에 파일위치 입력
                     col.insert(0, "\\".join([path, fname]))
                     
-                    df_result[i] = df_result[i].append(pd.DataFrame(data = [col], columns = df_col))
+                    df_result.append(pd.DataFrame(data = [col], columns = df_col))
 
-            i += 1
 
-    return df_result
+        result_q.put(df_result)
 
 def read_file(path, fname):
 
@@ -119,33 +134,38 @@ if __name__ == "__main__":
 
     keyword_list = f.read().split("\n")
 
+    start = time.time()
 
-    #리스트 분할
-    flist = []
-    for (path, dir, file) in os.walk('Search/내시피족'):
+    fname_q = Queue()
+    result_q = Queue()
+    p = Process(target=walk_file, args=(fname_q,))
+    p.start()
 
-        for fname in file:
-            flist.append([path, fname])
+    process_list = []
+    for N in range(0, 10):
+        process_list.append(Process(target=run_process, args=(fname_q, result_q, keyword_list,)))
+        process_list[N].start()
 
-    for (path, dir, file) in os.walk('Search/덕질메이트'):
+    p.join()
+    for p in process_list:
+        p.join()
 
-        for fname in file:
-            flist.append([path, fname])
+    while not result_q.empty():
+        print(result_q.get())
 
-    flist = divide_list(flist, 10)
+    '''
+    for keyword in keyword_list:
 
-    #함수에 넣기 전 전처리
-    #{argv : n, keyword : [], flist : []}
+        #n 개의 칼럼만 추출
+        col_cnt = int(sys.argv[1])
+        return_col = [str(i) for i in range(-col_cnt, col_cnt+1)]
+        return_col.insert(0, 'link')
+        return_col.insert(0, 'file')
 
-    map_val = []
-    for file_list in flist:
-        map_val.append({'argv' : sys.argv[-1], 'keyword' : keyword_list, 'flist' : file_list})
+        df_result = df_result[return_col]
+        df_result.to_csv(keyword + ".csv", encoding='utf-8-sig')
 
-    with Pool(10) as p:
-        result = p.map(run_process, map_val)
-
-    print(result)
-
+    ''' 
     '''
     for keyword in ['덕질Mate', '덕질메이트', '덕질mate', '덕 질 Mate', '덕 질 메이트']:
         print(keyword)
